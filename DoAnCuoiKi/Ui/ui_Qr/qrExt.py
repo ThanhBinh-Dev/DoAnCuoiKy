4919f42c44b60e72943fd141ecbd986d383df865
ThanhBinh-Dev <binh1610dtb@gmail.com>
2025-03-19 09:26:30 +0700

    fix QrExt

--- DoAnCuoiKi/Ui/ui_Qr/qrExt.py
+++ DoAnCuoiKi/Ui/ui_Qr/qrExt.py
@@ -1,10 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-
-<<<<<<< HEAD
 from DoAnCuoiKi.Ui.ui_DangNhap.login import Ui_MainWindow
-=======
-from DoAnCuoiKi.Ui.ui_DangNhap.login import Ui_LoginMainWindow
->>>>>>> 0e380858b4dd3a2896515ea5e2320330f20ed309
 from DoAnCuoiKi.Ui.ui_Qr.qr import Ui_MainWindow
 
 
@@ -13,30 +8,17 @@ class QrExt(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.show_login_window)
-<<<<<<< HEAD
         self.pushButton_caution.clicked.connect(self.QuayLaiPhieuXacNhan)
-=======
-        self.pushButton_caution.clicked.connect(self.return_PhieuXacNhan_window)
->>>>>>> 0e380858b4dd3a2896515ea5e2320330f20ed309
 
     def show_login_window(self):
         """Mở giao diện đăng nhập"""
         self.login_window = QMainWindow()
-<<<<<<< HEAD
         self.login_ui = Ui_MainWindow()
-=======
-        self.login_ui = Ui_LoginMainWindow()
->>>>>>> 0e380858b4dd3a2896515ea5e2320330f20ed309
         self.login_ui.setupUi(self.login_window)
         self.login_window.show()
         self.close()
 
-<<<<<<< HEAD
-
     def QuayLaiPhieuXacNhan(self):
-=======
-    def return_PhieuXacNhan_window(self):
->>>>>>> 0e380858b4dd3a2896515ea5e2320330f20ed309
         """Quay lại giao diện Phiếu Xác Nhận"""
         from DoAnCuoiKi.Ui.ui_PhieuXacNhan.PhieuXacNhanExt import PhieuXacNhanExt  # Import muộn
 
